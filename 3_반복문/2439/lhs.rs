736c19e655a37ff564c7fb5dcc4ec0a29e486c00
까미집사 <42618411+CaKlan@users.noreply.github.com>
2023-01-04 03:29:09 +0900

    BOJ_2439

---
+++ 3_반복문/2439/lhs.rs
@@ -0,0 +1,11 @@
+use std;
+
+fn main(){
+    let mut n = String::new();
+    std::io::stdin().read_line(&mut n).unwrap();
+    let n = n.trim().parse::<usize>().unwrap();
+    
+    for i in (0..n).rev(){ 
+        println!("{:*<n$}", " ".repeat(i));
+    }
+}
