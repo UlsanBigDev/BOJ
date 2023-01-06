afb26a6bf3397d9d39c604816688e1f1f8cc6b48
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-01-06 22:25:27 +0900

    BOJ_3003
    메모리 : 2020 KB
    속도 : 0 ms

---
+++ 1_입출력과_사칙연산/3003/ptk.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    int A = 1;
+    int B = 1;
+    int C = 2;
+    int D = 2;
+    int E = 2;
+    int F = 8;
+    int a, b, c, d, e, f;
+    std::cin >> a >> b >> c >> d >> e >> f;
+    std::cout << A - a << " " << B - b << " " << C - c << " " << D - d << " " << E - e << " " << F - f;
+
+}
