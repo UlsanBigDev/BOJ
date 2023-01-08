554328d32dfd7bcf105ac59bee7a0d04ff0c6317
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-01-08 14:13:18 +0900

    BOJ_25083
    메모리 : 2020 KB
    속도 : 0 ms

---
+++ 1_입출력과_사칙연산/25083/ptk.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "         ,r'\"7" << "\n";
+    std::cout << "r`-_   ,'  ,/" << "\n";
+    std::cout << " \\. \". L_r'" << "\n";
+    std::cout << "   `~\\/" << "\n";
+    std::cout << "      | " << "\n";
+    std::cout << "      |" << "\n";
+}
