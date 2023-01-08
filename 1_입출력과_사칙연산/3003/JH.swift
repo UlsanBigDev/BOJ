13f972e64e4e264b1b335123bfbb112ce8ff4660
Daram <89921213+Daram0o0@users.noreply.github.com>
2023-01-08 16:48:36 +0900

    BOJ_3003

---
+++ 1_입출력과_사칙연산/3003/JH.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+var a=readLine()!
+var arr=a.components(separatedBy:" ")
+let A=Int(arr[0])!
+let B=Int(arr[1])!
+let C=Int(arr[2])!
+let D=Int(arr[3])!
+let E=Int(arr[4])!
+let F=Int(arr[5])!
+
+print("\(1-A) \(1-B) \(2-C) \(2-D) \(2-E) \(8-F)")
