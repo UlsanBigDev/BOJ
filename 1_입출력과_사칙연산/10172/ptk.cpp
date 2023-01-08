fbe1e5d35ceeafdb0b1644656a35cf3e92b0bef1
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-01-08 13:59:09 +0900

    BOJ_10172
    메모리 : 2020 kB
    속도 : 0 ms

---
+++ 1_입출력과_사칙연산/10172/ptk.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() 
+{
+    std::cout << "\|\\_\/\|" << "\n";
+    std::cout << "\|q p\|   \/\}" << "\n";
+    std::cout << "\( 0 \)\"\"\"\\" << "\n";
+    std::cout << "\|\"^\"\`    \|" << "\n";
+    std::cout << "\|\|_\/\=\\\\__\|" << "\n";
+}
