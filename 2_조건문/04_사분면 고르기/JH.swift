d1606dd672730423b16095b189d1afc2d84f4cf8
Jiho <89921213+Daram0o0@users.noreply.github.com>
2023-01-09 18:59:03 +0900

    BOJ_14681

---
+++ 2_조건문/04_사분면 고르기/JH.swift
@@ -0,0 +1,17 @@
+import Foundation
+let a=Int(readLine()!)!
+let b=Int(readLine()!)!
+
+if a>0{
+    if b>0{
+        print("1")
+    }else{
+        print("4")
+    }
+}else{
+    if b>0{
+        print("2")
+    }else{
+        print("3")
+    }
+}
