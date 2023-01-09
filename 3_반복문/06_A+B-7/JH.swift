837ad4a521839b679601c992724b68a1c1365d36
Jiho <89921213+Daram0o0@users.noreply.github.com>
2023-01-10 01:09:16 +0900

    BOJ_11021

---
+++ 3_반복문/06_A+B-7/JH.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+let T = Int(readLine()!)!
+
+for i in 0..<T {
+    let arr = readLine()!.components(separatedBy: " ").map{Int($0)!}
+    print("Case #\(i+1): \(arr[0]+arr[1])")
+}
