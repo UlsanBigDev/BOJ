d9ad9f047be62a7f0442d2c58daafc6ca9138869
JIWAN KIM <109573350+jiwan0123@users.noreply.github.com>
2023-01-11 01:42:35 +0900

    BOJ_13156
    메모리 : 13156KB 시간 : 272ms

---
+++ 3_반복문/05_빠른_A+B/kjw.rs
@@ -0,0 +1,20 @@
+use io::Write;
+use std::io;
+fn main() {
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).unwrap();
+    let num: i32 = a.trim().parse().unwrap();
+    let stdout = io::stdout();
+    let mut out = io::BufWriter::new(stdout.lock());
+    for _ in 0..num {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let result: Vec<i32> = input
+            .as_mut_str()
+            .split_whitespace()
+            .map(|input| input.parse().unwrap())
+            .collect();
+        writeln!(out, "{}", result[0] + result[1]).unwrap();
+    }
+}
