26c4825e26dd73c19734489fe683ae59bb6d7e77
까미집사 <42618411+CaKlan@users.noreply.github.com>
2023-01-14 23:57:51 +0900

    BOJ_3052

--- 4_1 차원 배열/06_나머지/lhs.rs
+++ 4_1 차원 배열/06_나머지/lhs.rs
@@ -14,3 +14,22 @@ fn main(){
     }
     println!("{}", vec.len());
 }
+
+// bit masking
+
+use std;
+
+fn main(){
+    let stdin = std::io::stdin();
+    
+    let mut vec = [false; 43];
+    for _ in 0..10{
+        let mut buf = String::new();
+        stdin.read_line(&mut buf).unwrap();
+        let buf = buf.trim().parse::<u32>().unwrap() % 42;
+        
+        vec[buf as usize] |= true; //or 후 대입
+    }
+
+    println!("{}", vec.iter().filter(|&n| *n == true).count());
+}
