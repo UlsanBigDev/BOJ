f0e15684b101799582f5612e0205c385c84e8277
Hun <84387602+Hun-322@users.noreply.github.com>
2023-01-15 23:27:42 +0900

    BOJ_11021

---
+++ 3_반복문/06_A+B-7/Hun.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+var loop = Int(readLine()!)!
+
+for i in 1...loop {
+    let arr = readLine()!.components(separatedBy: " ").map{Int($0)!}
+
+    print("Case #\(i): \(arr[0] + arr[1])")
+}
