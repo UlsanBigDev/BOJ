2b5ec6306705d75cd49bcdf62840a2d64c407f29
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-01-21 21:01:53 +0900

    BOJ_11021
    메모리 : 2020 KB
    속도 : 0 ms

---
+++ 3_반복문/06_A+B-7/ptk.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    std::cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+    std::cin >> a;
+    for (int i=1; i<=a; i++)
+    {
+        int x, y;
+        std::cin >> x >> y;
+        std::cout << "Case #" << i << ": " << x + y << "\n";    
+    }
+}
