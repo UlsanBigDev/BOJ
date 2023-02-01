14f7e762e15092cdd232113ec954d65627b3447f
Jiho <89921213+Daram0o0@users.noreply.github.com>
2023-02-01 20:34:30 +0900

    BOJ_15552

---
+++ 03_반복문/05_빠른_A+B/JH.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+}
+
+let fIO = FileIO()
+
+let N = fIO.readInt()
+
+for _ in 0..<N{
+    let A = fIO.readInt()
+    let B = fIO.readInt()
+    print(A+B)
+}
