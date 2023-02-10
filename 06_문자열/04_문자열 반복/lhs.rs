57981bfb718ad2e56ec751c4a6c8a62152891f00
까미집사 <42618411+CaKlan@users.noreply.github.com>
2023-02-10 21:01:00 +0900

    BOJ_2675

--- 06_문자열/04_문자열 반복/lhs.rs
+++ 06_문자열/04_문자열 반복/lhs.rs
@@ -1,1 +1,25 @@
+fn main() {
+    let mut buf = String::new();
+    let stdin = std::io::stdin();
+    stdin.read_line(&mut buf).unwrap();
 
+    let n = buf.clone().trim().parse::<i32>().unwrap();
+    buf.clear();
+
+    for _ in 0..n{
+        stdin.read_line(&mut buf).unwrap();
+        let mut wh = buf.trim().split_whitespace();
+
+        let count = wh.next().unwrap();
+        let text = wh.next().unwrap();
+
+        let text = text.chars().collect::<Vec<char>>();
+        for i in 0..text.len(){
+            for _ in 0..count.parse::<usize>().unwrap(){
+                print!("{}", text[i]);
+            }
+        }
+        println!("");
+        buf.clear();
+    }
+}
