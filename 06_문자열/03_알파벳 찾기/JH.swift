e7c2d5831cf838498f2c69df6247e873d528fe69
Jiho <89921213+Daram0o0@users.noreply.github.com>
2023-02-12 13:25:05 +0900

    BOJ_10809

--- 06_문자열/03_알파벳 찾기/JH.swift
+++ 06_문자열/03_알파벳 찾기/JH.swift
@@ -1,11 +1,10 @@
 var str = Array(readLine()!)
-var arr = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
+var arr = Array(repeating: -1, count: 26)
 var num : Int = 0
 
 for i in str{
     num = Int(i.asciiValue!-97)
     arr[num] = str.firstIndex(of: i) ?? 0
-    
 }
 
 for i in arr{
