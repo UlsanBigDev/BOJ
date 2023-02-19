ce0f657181b7365a52074e1c362dd4269be901b2
까미집사 <42618411+CaKlan@users.noreply.github.com>
2023-02-19 21:16:14 +0900

    Update lhs.rs

--- 07_기본수학1/04_달팽이는올라가고싶다/lhs.rs
+++ 07_기본수학1/04_달팽이는올라가고싶다/lhs.rs
@@ -3,24 +3,10 @@ fn main(){
     let stdin = std::io::stdin();
     stdin.read_line(&mut buf).unwrap();
 
-    let mut n = buf.clone().trim().parse::<i32>().unwrap();
-    buf.clear();
+    let t = buf.trim().split_whitespace().map(|v| v.parse::<f64>().unwrap()).collect::<Vec<f64>>();
 
-    for _ in 0..n{
-        stdin.read_line(&mut buf).unwrap();
-        let t = buf.trim().split_whitespace().map(|v| v.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        let (h, w, v) = (t[0], t[1], t[2]);
+    let (a, b, v) = (t[0], t[1], t[2]);
 
-        let mut result = 0;
-        
-        if v % h == 0{
-            result += h * 100;
-        }else{
-            result += (v % h) * 100;
-        }
-
-        result += ((v-1) / h) as i32 + 1;
-        println!("{result}");
-        buf.clear();
-    }
+    let result = (((v - a)/ (a - b)) as f64).ceil() + 1_f64;
+    print!("{result}");
 }
